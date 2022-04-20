533c7d641705dfa8388bf23d3eddf894174e77a0
qmsteven <74738665+qmsteven@users.noreply.github.com>
2022-04-20 08:34:21 -0400

    Added toolbar

--- app/src/main/java/com/company/myapp/MainActivity.kt
+++ app/src/main/java/com/company/myapp/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.company.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.company.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val textView = binding.textView
+        val button = binding.button
+
+        button.setOnClickListener {
+            if (textView.text == "Hello World!")
+                textView.text = "You did it!"
+            else
+                textView.text = "Hello World!"
+        }
+        setSupportActionBar(binding.mainToolBar.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home -> {
+//                startActivity(Intent(applicationContext, MainActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
